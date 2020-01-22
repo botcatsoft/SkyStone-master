f1f9cbddaa7f49587203b665681ac67e6395f39d
ejpre <ejpregerson@hotmail.com>
2020-01-22 08:04:20 -0800

    added angle tester

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testers/AngleTester.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode.Testers;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+@TeleOp(name = "Angle test") public class AngleTester extends LinearOpMode {
+    public void runOpMode(){
+        BNO055IMU imu = hardwareMap.get(BNO055IMU.class, "imu");
+        BNO055IMU.Parameters param = new BNO055IMU.Parameters();param.loggingEnabled = false;
+        param.angleUnit = BNO055IMU.AngleUnit.RADIANS;
+        param.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        imu.initialize(param);
+
+
+
+        waitForStart();
+        while(opModeIsActive()){
+
+            Orientation angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+
+            telemetry.addData("Angle: ", angles.firstAngle);
+            telemetry.update();
+        }
+    }
+}
